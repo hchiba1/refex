0c7f28217a896424bc064c451475512449a32410
Hirokazu Chiba <hchiba1@gmail.com>
2020-12-22 14:04:37 +0900

    update tissue_specific.py

--- tissue_specific.py
+++ tissue_specific.py
@@ -1,9 +1,8 @@
 #!/usr/bin/env python3
-import sys
 import argparse
 
 parser = argparse.ArgumentParser(description='RDFize RefEx')
-parser.add_argument('data', help='hOP data directory')
+parser.add_argument('input_file', help='RefEx tissue specific data file')
 args = parser.parse_args()
 
 print('@prefix refexo: <http://purl.jp/bio/01/refexo#>')
@@ -11,22 +10,14 @@ print('@prefix affy: <http://identifiers.org/affy.probeset/>')
 print('@prefix refseq: <http://identifiers.org/refseq/>')
 print()
 
-# def print_profile(group_no, label, prifoke, member):
-#     print(f'group:{group_no} a orth:OrthologsCluster ;')
-#     print(f'    orth:inDataset <https://orth.dbcls.jp/data/hop/> ;')
-
-# def print_profile(tissues):
-#     for tissue in tissues:
-#         print(' refexo:overExpressedIn ')
-#         print(' '.join(tissues))
-
-fp = open(args.data, 'r')
+fp = open(args.input_file, 'r')
 checked_header = False
 col_name = ''
 prefix_name = ''
 for line in fp:
     fields = line.strip().split('\t')
     col_name = fields[0]
+    
     if not checked_header:
         checked_header = True
         if col_name == 'Affymetrix_probesetID':
@@ -34,6 +25,7 @@ for line in fp:
         elif col_name == 'NCBI_RefSeqID':
             prefix_name = 'refseq'
         continue
+    
     for i in range(2, len(fields)):
         val = fields[i]
         if val == '1':
