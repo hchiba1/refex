5a1802b6d19e5841cafa7c5d17ff2ade5035f76b
Hirokazu Chiba <hchiba1@gmail.com>
2020-12-26 06:44:33 +0900

    remove unnecessary prefix

--- tissue_specific.py
+++ tissue_specific.py
@@ -5,11 +5,6 @@ parser = argparse.ArgumentParser(description='RDFize RefEx tissue specificity da
 parser.add_argument('input_file', help='RefEx tissue specificity data file')
 args = parser.parse_args()
 
-print('@prefix refexo: <http://purl.jp/bio/01/refexo#>')
-print('@prefix affy: <http://identifiers.org/affy.probeset/>')
-print('@prefix refseq: <http://identifiers.org/refseq/>')
-print()
-
 fp = open(args.input_file, 'r')
 checked_header = False
 prefix = ''
@@ -18,10 +13,14 @@ for line in fp:
     
     if not checked_header:
         checked_header = True
+        print('@prefix refexo: <http://purl.jp/bio/01/refexo#>')
         if fields[0] == 'Affymetrix_probesetID':
             prefix = 'affy'
+            print('@prefix affy: <http://identifiers.org/affy.probeset/>')
         elif fields[0] == 'NCBI_RefSeqID':
             prefix = 'refseq'
+            print('@prefix refseq: <http://identifiers.org/refseq/>')
+        print()
         continue
     
     for i in range(2, len(fields)):
